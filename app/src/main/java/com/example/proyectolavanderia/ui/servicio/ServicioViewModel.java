05b53c7be37e984d407d1d434b482bd2fa1d8f9b
Gianmarco Pillaca <gpillaca91@gmail.com>
2019-11-08 12:35:43 -0500

    Interface pagosOnline

---
+++ app/src/main/java/com/example/proyectolavanderia/ui/servicio/ServicioViewModel.java
@@ -0,0 +1,7 @@
+package com.example.proyectolavanderia.ui.servicio;
+
+import androidx.lifecycle.ViewModel;
+
+public class ServicioViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
